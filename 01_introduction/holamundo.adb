ad6ef51b72d3149db69282c25e203a6c08b1c6a8
Timmy Jose <zoltan.jose@gmail.com>
2020-05-04 10:52:58 +0530

    Introduction.

---
+++ 01_introduction/holamundo.adb
@@ -0,0 +1,11 @@
+-- hola_mundo.adb
+
+with Ada.Text_IO;
+
+procedure HolaMundo is
+begin
+   Ada.Text_IO.Put_Line ("Hola, mundo!");
+   Ada.Text_IO.Put ("It's a wonderful day!");
+   Ada.Text_IO.New_Line;
+end HolaMundo;
+
---
+++ 01_introduction/lab.adb
@@ -0,0 +1,21 @@
+with Ada.Text_IO;
+
+procedure Lab is
+begin
+   Ada.Text_IO.Put_Line ("#############################################################");
+   Ada.Text_IO.Put_Line ("#############################################################");
+   Ada.Text_IO.Put_Line ("##                                                         ##");
+   Ada.Text_IO.Put_Line ("##          OOOOOOOOOOO  OOOOOOOOOOO  OOOOOOOOOOO          ##");
+   Ada.Text_IO.Put_Line ("##          OO       OO  OO       OO  OO       OO          ##");
+   Ada.Text_IO.Put_Line ("##          OO       OO  OO       OO  OO       OO          ##");
+   Ada.Text_IO.Put_Line ("##          OO       OO  OO       OO  OO       OO          ##");
+   Ada.Text_IO.Put_Line ("##          OOOOOOOOOOO  OO       OO  OOOOOOOOOOO          ##");
+   Ada.Text_IO.Put_Line ("##          OO       OO  OO       OO  OO       OO          ##");
+   Ada.Text_IO.Put_Line ("##          OO       OO  OO       OO  OO       OO          ##");
+   Ada.Text_IO.Put_Line ("##          OO       OO  OO       OO  OO       OO          ##");
+   Ada.Text_IO.Put_Line ("##          OO       OO  OOOOOOOOOO   OO       OO          ##");
+   Ada.Text_IO.Put_Line ("##                                                         ##");
+   Ada.Text_IO.Put_Line ("#############################################################");
+   Ada.Text_IO.Put_Line ("#############################################################");
+end Lab;
+
