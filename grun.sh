8c8f3f8203724ba8b48c48b42329c58c2f786d62
Timmy Jose <zoltan.jose@gmail.com>
2020-05-05 15:31:04 +0530

    Fixed scripts to handle cleanup even with Ctrl-C

--- grun.sh
+++ grun.sh
@@ -3,12 +3,26 @@
 file="$1"
 filename="${file%.*}"
 
+# cleanup even if Ctrl-C is pressed during execution
+trap cleanup_and_exit INT
+
+function cleanup() {
+  rm -f ${filename}
+  rm -f ${filename}.ali
+  rm -f ${filename}.o
+  rm -f b~${filename}.adb
+  rm -f b~${filename}.ads
+  rm -f b~${filename}.ali
+  rm -f b~${filename}.o
+}
+
+function cleanup_and_exit() {
+  echo "Detected Ctrl-C... cleaning up before exit"
+  cleanup
+}
+
 gnatmake -g ${file} && ./${filename}
 
-rm -f ${filename}
-rm -f ${filename}.ali
-rm -f ${filename}.o
-rm -f b~${filename}.adb
-rm -f b~${filename}.ads
-rm -f b~${filename}.ali
-rm -f b~${filename}.o
+# normal exit
+cleanup
+
